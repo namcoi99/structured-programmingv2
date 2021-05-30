ab00758455302828538705ff1bd79f70b8c41a9e
namnguyenn99 <namnd.vnist@gmail.com>
2021-05-31 00:35:40 +0700

    axios config

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import config from './config';
 
 export default axios.create({
+    // Tạo url cơ sở khi gửi request cho backend
     baseURL: config.rootPath,
     withCredentials: true
 });
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 export default {
     // rootPath: process.env.REACT_APP_STATE === 'localhost' ? 'http://localhost:5000' : ''
 
-    // Đường dẫn tới url của backend
+    // Đường dẫn tới url của backend 
     rootPath: 'http://localhost:5000',
     
 }
