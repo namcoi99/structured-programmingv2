1db939b358017dde6a5b921c47bd9d335a148e8b
tatuan19 <trananhtuan12a10@gmail.com>
2021-06-17 21:09:42 +0700

    Update database

--- sql/data.sql
+++ sql/data.sql
@@ -1,39 +1,33 @@
 ﻿-- Product
--- Pizza
+-- Shirt
 
 use WebCSDL
 
--- Áo nữ
+select * from Product
+
+-- Shirt
+INSERT INTO Product
+VALUES 
+('SH01',N'Blue Tshirt','299000',N'ClownZ x Collectors Tie Dye Tee Flame Dices','shirt1','Shirt','6'),
+('SH02',N'Black Tshirt','145000',N'Pepsi x ClownZ Laugh Now T-shirt','shirt2','Shirt','7'),
+('SH03',N'Oversize T-shirt','250000',N'Pepsi x ClownZ Disorted T-shirt','shirt3','Shirt','8'),
+('SH04',N'White T-shirt','169000',N'ClownZ Terraria Camouflage T-shirt','shirt4','Shirt','0'),
+('SH05',N'Brumano','230000',N'Basebal Typography T-shirt','shirt5','Shirt','15')
+
+-- Pants
 INSERT INTO Product
 VALUES
-('ATL0446',N'Áo thun giữ nhiệt nữ Emspo cổ 6F dáng suông vừa trắng','229000',N'Áo thun giữ nhiệt nữ được Emspo chăm chút từ khâu thiết kế đến khâu gia công với mong muốn đem đến một sản phẩm tốt nhất cho khách hàng của mình','hbg8','Áo nữ','5'),
-('TC2018002WH',N'Áo thun len tay dài cổ tròn dáng suông The Cosmo (trắng)','99000',N'Áo thun len tay dài, cổ tròn, dáng suông, có lớp lót thun mỏng phía trong. Áo được may trên chất liệu mềm mịn và thoáng mát','hbg9','Áo nữ','7'),
-('14109V620',N'Áo sơ mi kiểu TosonFashion cổ đức xếp ly gấu thắt nơ ren','149000',N'Áo sơ mi kiểu TosonFashion cổ đức xếp ly gấu thắt nơ ren được may từ chất liệu Chiffon đem đến hình ảnh một cô nàng dịu dàng và nữ tính cho phái đẹp. Áo sơ mi kiểu TosonFashion thiết kế cổ đức, thắt nơ eo, mang đến hình ảnh dịu dàng nữ tính cùng cảm giác thoải mái khi mặc','hbg10','Áo nữ','2')
+('PA01',N'Nike x Undercover','299000',N'The latest partnership between Jun Takahashi and Nike redefines wet-weather gear.','pants1','Pants','6'),
+('PA02',N'Jordan AJ11 Graphic','145000',N'Make a match in the Jordan AJ11 Graphic Fleece Trousers, featuring prints inspired by the iconic game shoe.','pants2','Pants','7'),
+('PA03',N'Jordan AJ4','250000',N'The Jordan AJ4 Graphic Fleece Trousers are cuffed around the ankles so they dont get in the way of your Js.','pants3','Pants','8'),
+('PA04',N'Nike ACG Polartec Wolf Tree','169000',N'Wet weather doesnt mean you have to stop exploring.','pants4','Pants','0'),
+('PA05',N'Nike Sportswear Windrunner','230000',N'The Nike Sportswear Windrunner Trousers are made from lightweight nylon fabric for a relaxed, comfortable feel.','pants5','Pants','15')
 
--- Áo khoác và áo len nữ
+-- Bag
 INSERT INTO Product
 VALUES 
-('4018',N'Áo khoác nữ cổ chui hoodie Phúc An màu đen','159000',N'Áo khoác 4018 là mẫu áo khoác cổ chui cổ điển không lỗi mốt. Vì không có nút cài hay dây kéo mẫu 4018 trông rất gọn gàng như phong cách minimalist','milktea1','Áo khoác và áo len nữ','25'),
-('V65P18T023-GOS14',N'Áo khoác phao nữ Vienne Tran cổ B túi chéo','299000',N'Áo khoác phao nữ Vienne Tran cổ B túi chéo có chất liệu phao trần bông miếng nhẹ, giữ ấm tốt. Thiết kế áo 2 lớp, có lót. Một chút khéo léo trong khâu mix & match hẳn sẽ giúp bạn trở nên duyên dáng hơn, xinh đẹp hơn trong mắt tất cả mọi người xung quanh','milktea2','Áo khoác và áo len nữ','33'),
-('ATS0370',N'Áo len nữ cổ trụ Emspo bo gấu 2 màu','167300',N'Chất vải được Emspo chọn lọc kỹ lưỡng, thiết kế đơn giản nhưng cũng không kém phần thời trang.','milktea3','Áo khoác và áo len nữ','10'),
-('VT0158 LT185101581',N'Áo Blazer phối lưới thêu hoa Bella Moda','499000',N'Áo Blazer phối lưới thêu hoa Bella Moda với thiết kế trẻ trung, thanh lịch hợp thời trang, giúp cho vẻ ngoài của phái đẹp chỉn chu và lịch sự hơn mỗi khi đến nơi làm việc','milktea4','Áo khoác và áo len nữ','5'),
-('CA20090-22',N'Áo khoác gió nữ Chelle lót lưới bo gấu hồng','265500',N'Áo khoác gió nữ Chelle lót lưới bo gấu Hồng được may từ chất liệu cản gió tốt, khó ngấm nước, là 1 chất liệu tuyệt vời cho mùa thu đông, mang đến sự tiện ích mà vẫn nhẹ nhàng cho người mặc','milktea5','Áo khoác và áo len nữ','21'),
-('15032',N'Áo khoác da lộn Trali nâu trẻ trung','1150000',N'Áo khoác da lộn Trali nâu trẻ trung là một item thời trang được nhiều nàng yêu thích lựa chọn để diện vào những ngày đặc biệt hay đến những nơi quan trọng. Áo blazer luôn "được lòng" các chị em phụ nữ bởi kiểu dáng hiện đại, thời thượng, không kém phần "sang chảnh" và chẳng hề "kén" người mặc. Bạn cũng có thể làm mới phong cách thời trang mùa đông của mình với áo blazer nữ giúp giữ ấm cơ thể cho những ngày se lạnh!','milktea6','Áo khoác và áo len nữ','45'),
-('ALS0010',N'Áo len mỏng Emspo dáng ôm kẻ ngang','89000',N'Emspo sử dụng chất liệu được lựa chọn kỹ lưỡng nhằm mang đến sự thoải mái, dễ chịu nhất cho người mặc cũng như đem đến những trải nghiệm tuyệt vời cho khách hàng khi lựa chọn sản phẩm','milktea7','Áo khoác và áo len nữ','5'),
-('AVL0555',N'Áo vest nữ cổ V Emspo bẻ ve phần trên túi giả 2 bên','329000',N'Sản phẩm của thời trang Emspo luôn là sự kết hợp hoàn hảo giữa chất liệu, kiểu dáng và màu sắc đem lại cho người mặc cảm giác tự tin - thoải mái nhất dù đi đến bất cứ đâu, công sở, dạo phố hay mặc nhà','milktea8','Áo khoác và áo len nữ','7'),
-('62016E949',N'Áo len cổ đức bổ trụ TosonFashion tay và cổ phối màu','199000',N'Áo len cổ đức bổ trụ TosonFashion tay và cổ phối màu được may từ chất liệu len với kiểu dáng đơn giản trẻ trung, nữ tính. Chúng không những êm ái, mềm mại, giữ ấm tuyệt vời cho cơ thể của bạn mà còn có thiết kế trẻ trung, sành điệu dễ dàng phối hợp với mọi kiểu trang phục khác nhau. Hãy làm sáng bừng tủ đồ của bạn trong mùa thu đông năm nay bằng cách lựa chọn một chiếc áo len đến từ TosonFashion nhé!','milktea9','Áo khoác và áo len nữ','29'),
-('L65P18T032-S1800',N'Áo phao cổ dựng khóa đồng Lamer','199000',N'Áo phao cổ dựng khóa đồng Lamer với chất liệu phao siêu nhẹ, cản gió, giữ ấm tốt. Áo 2 lớp, có lót. Cổ dựng, dáng ngắn, dài tay, giữ ấm tốt. Màu sắc trung tính dễ phối đồ','milktea10','Áo khoác và áo len nữ','108')
-
--- Áo thun nam
-INSERT INTO Product
-VALUES 
-('ATS0357',N'Áo thun nam tay ngắn Emspo dáng suông in hình trẻ trung','269000',N'Áo thun nam tay ngắn Emspo dáng suông in hình trẻ trung được may từ chất liệu Cotton đem đến hình ảnh trẻ trung năng động cho phái mạnh','pizza1','Áo thun nam','6'),
-('ATS0250',N'Áo thun nam tay ngắn Emspo cổ tròn','239000',N'Áo thun nam tay ngắn Emspo cổ tròn được may từ chất liệu thun cotton, thiết kế đơn giản đem lại cho người mặc cảm giác thoải mái dễ chịu','pizza2','Áo thun nam','7'),
-('TC1021065R3RE',N'Áo thun nam THE COSMO JOY TEE kẻ sọc đỏ','99000',N'Áo thun nam THE COSMO JOY TEE kẻ sọc đỏ cổ tròn, tay ngắn, được may từ chất liệu 100% cotton mang đến độ thấm hút mồ hôi tuyệt đối và cảm giác thoải mái, thoáng mát để các chàng thỏa thích vận động suốt cả ngày','pizza3','Áo thun nam','8'),
-('Ao.Polo.VIC.Ghi',N'Áo thun polo nam Vicci cộc tay màu ghi','150000',N'Áo thun polo nam Vicci cộc tay màu ghi được may từ chất liệu vải thun cao cấp, đường may tỉ mỉ, mang đến độ bền cao cho người sử dụng','pizza4','Áo thun nam','0'),
-('TV11A2029',N'Áo thun nam Polo tay ngắn Hàn Quốc The Shirts Studio','399000',N'Áo thun nam Polo tay ngắn Hàn Quốc The Shirts Studio được may từ chất liệu vải polyester và cotton mềm, mát, thoáng khí, thấm hút mồ hôi tốt','pizza5','Áo thun nam','15'),
-('TC1021077BL',N'Áo thun nam tay dài The Cosmo Austin Polo cổ polo đơn sắc','169000',N'Áo thun nam tay dài The Cosmo Austin Polo cổ polo đơn sắc mang đến vẻ ngoài hiện đại và trẻ trung cho các chàng trai sành điệu với thiết kế cơ bản quen thuộc','pizza6','Áo thun nam','0'),
-('ATL0389',N'Áo thun polo nam Emspo dài tay kẻ sọc','299000',N'Áo thun nam Emspo mang phong cách khỏe khoắn, năng động với chất liệu thun co giãn và thấm hút mồ hôi tốt là 1 lựa chọn không thể thiếu trong tủ đồ mùa thu đông của mọi gia đình','pizza7','Áo thun nam','0'),
-('ATS0295',N'Áo thun nam cổ tròn, tay ngắn, in hoạ tiết Emspo','269000',N'Áo thun Emspo cổ tròn, tay ngắn, in hình trước ngực đơn giản mang phong cách khỏe khoắn, năng động với chất liệu thun co giãn và thấm hút mồ hôi tốt là 1 lựa chọn không thể thiếu trong tủ đồ mùa hè của mọi gia đình','pizza8','Áo thun nam','5'),
-('MBT20111GE',N'Áo thun polo nam Mint Basic trẻ trung - xanh lá cây','215000',N'Áo thun polo trên nền thun cotton mềm mại có độ thấm hút cao, màu sắc trơn dễ dàng phối cùng nhiều kiểu quần khác nhau là gợi ý không thể hoàn hảo hơn cho cuối tuần của bạn','pizza9','Áo thun nam','7'),
-('TC1021074R1WH',N'Áo polo nam THE COSMO RALPH SHIRT trơn màu','189000',N'Ralph Polo Shirt được may từ chất liệu 100% cotton cao cấp, đảm bảo sự thấm hút mồ hôi tối đa cho chàng thêm tự tin trong các hoạt động hàng ngày','pizza10','Áo thun nam','0')
+('BA01',N'Tech Pack','78000',N'The Tech Pack is designed to organize and protect all your tech essentials.','bag1','Bag','25'),
+('BA02',N'Gucci Womens Crocodile Bamboo Convertible Handbag','58000',N'This handbag is as beautiful as it is stunning. The crocodile bears shades of both vanilla cream and a light shade grey.','bag2','Bag','33'),
+('BA03',N'Judith Leiber Nabila Crocodile Exotic Shoulder Bag Mocha','46000',N'When you need a small purse but dont want to sacrifice style, this one is the way to go.','bag3','Bag','10'),
+('BA04',N'Light Pink Crocodile Bamboo Convertible Handbag','42000',N'This small bag has a warm, friendly tone. The crocodile texture is soft pink, giving it a more natural and organic feel.','bag4','Bag','5'),
+('BA05',N'Small Pink Crocodile Bamboo Convertible Handbag','69000',N'When subtlety isnt your specialty and understated isnt your style, then even your small purses need to be loud.','bag5','Bag','21')
